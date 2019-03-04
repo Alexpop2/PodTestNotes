4fe7a2561bc44ca903fc87b5414ec38a5a9746ef
Alexei Poponin <alexqw1@ya.ru>
2019-03-04 19:53:43 +0300

    Initial Commit

---
+++ PodTestNotes/PodTestNotes.h
@@ -0,0 +1,19 @@
+//
+//  PodTestNotes.h
+//  PodTestNotes
+//
+//  Created by Рабочий on 04/03/2019.
+//  Copyright © 2019 Рабочий. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PodTestNotes.
+FOUNDATION_EXPORT double PodTestNotesVersionNumber;
+
+//! Project version string for PodTestNotes.
+FOUNDATION_EXPORT const unsigned char PodTestNotesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodTestNotes/PublicHeader.h>
+
+
